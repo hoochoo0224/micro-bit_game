dba65105817b3e98c1d7ef833a311a4ed5f7925e
Jooowon Ko <83201165+hoochoo0224@users.noreply.github.com>
2024-04-09 19:04:52 +0900

    main.ts, main.py 업데이트하기

--- main.py
+++ main.py
@@ -11,24 +11,27 @@ player_pos = {"x": 3, "y": 3}
 
 game_map = ""
 
-for i in range(pos.length):
-    for j in range(5):
-        if j != 4:
-            game_map += " "
-        if j == player_pos["x"] and i == player_pos["y"]:
-            game_map += player
-        else:
-            game_map += pos[i][j]
-
-    if i != (pos.length-1):
-        game_map += "\n"
-
-
-def on_button_pressed_a():
-    player_pos
-
 def on_forever():
+    for i in range(pos.length):
+        for j in range(5):
+            if j != 4:
+                game_map += " "
+            if j == player_pos["x"] and i == player_pos["y"]:
+                game_map += player
+            else:
+                game_map += pos[i][j]
+
+        if i != (pos.length-1):
+            game_map += "\n"
+
     basic.show_leds(game_map)
 
+def on_button_pressed_a():
+    player_pos["x"] -= 1
+
+def on_button_pressed_b():
+    player_pos["x"] += 1
+
 input.on_button_pressed(Button.A, on_button_pressed_a)
+input.on_button_pressed(Button.B, on_button_pressed_b)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -6,27 +6,30 @@ let player_pos = {
 }
 
 let game_map = ""
-for (let i = 0; i < pos.length; i++) {
-    for (let j = 0; j < 5; j++) {
-        if (j != 4) {
-            game_map += " "
-        }
-        
-        if (j == player_pos["x"] && i == player_pos["y"]) {
-            game_map += player
-        } else {
-            game_map += pos[i][j]
-        }
-        
-    }
-    if (i != pos.length - 1) {
-        game_map += "\n"
-    }
-    
-}
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    player_pos
+    player_pos["x"] -= 1
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    player_pos["x"] += 1
 })
 basic.forever(function on_forever() {
+    for (let i = 0; i < pos.length; i++) {
+        for (let j = 0; j < 5; j++) {
+            if (j != 4) {
+                let game_map += " "
+            }
+            
+            if (j == player_pos["x"] && i == player_pos["y"]) {
+                game_map += player
+            } else {
+                game_map += pos[i][j]
+            }
+            
+        }
+        if (i != pos.length - 1) {
+            game_map += "\n"
+        }
+        
+    }
     basic.showLeds(game_map)
 })
